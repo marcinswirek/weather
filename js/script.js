e34664261b3e823b8291e268ea4200b8875e377a
Marcin Świrek <swirek.marcin@gmail.com>
2018-09-25 17:52:05 +0200

    Add temperature and weather display

--- js/script.js
+++ js/script.js
@@ -1,19 +1,30 @@
-var weatherUrl =
-  'https://api.openweathermap.org/data/2.5/weather?q=Sydney&units=metric&appid=672e93b94fcef6c0d2365b6ee1fe7e99';
-var request = new XMLHttpRequest();
+// let city = document.getElementById('cityName').value;
+let city = 'Kraków';
+console.log(city);
+let weatherUrl =
+  'https://api.openweathermap.org/data/2.5/weather?q=' +
+  city +
+  '&units=metric&appid=672e93b94fcef6c0d2365b6ee1fe7e99';
+let request = new XMLHttpRequest();
 
 request.open('GET', weatherUrl, true);
 
 request.onload = function() {
+  let data = JSON.parse(request.responseText);
+  let icon =
+    'https://openweathermap.org/img/w/' + data.weather[0].icon + '.png';
+  //let siteIcon = document.getElementById('weather-icon');
+  let temp = data.main.temp;
+  let weather = data.weather[0].main;
+
   if (request.status >= 200 && request.status < 400) {
     console.log('All is ok');
-    var data = JSON.parse(request.responseText);
+
     console.log(data);
-    var icon =
-      'https://openweathermap.org/img/w/' + data.weather[0].icon + '.png';
-    console.log(icon);
-    let siteIcon = document.getElementById('weather-icon');
+
     document.getElementById('weather-icon').src = icon;
+    document.getElementById('temp').innerText = temp;
+    document.getElementById('weather').innerText = weather;
   } else {
     console.log('Server error');
   }
